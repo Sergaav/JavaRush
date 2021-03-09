3d490a85dba48d8562ad4b1a42da76cca5f3f8dd
Andrii Serha <sergaav@gmail.com>
2021-03-09 14:51:26 +0200

    Commit 090321(3)

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task32.task3209;
 
 import com.javarush.task.task32.task3209.listeners.FrameListener;
+import com.javarush.task.task32.task3209.listeners.TabbedPaneChangeListener;
 
 import javax.swing.*;
 import java.awt.*;
@@ -29,6 +30,16 @@ public class View extends JFrame implements ActionListener {
     }
 
     public void initEditor() {
+        htmlTextPane.setContentType("text/html");
+        JScrollPane jScrollPane = new JScrollPane(htmlTextPane);
+        tabbedPane.add("HTML", jScrollPane);
+        JScrollPane plainScrollPane = new JScrollPane(plainTextPane);
+        tabbedPane.addTab("Текст", plainScrollPane);
+        TabbedPaneChangeListener changeListener = new TabbedPaneChangeListener(this);
+        tabbedPane.addChangeListener(changeListener);
+        tabbedPane.setPreferredSize(new Dimension(300,300));
+        getContentPane().add(tabbedPane,BorderLayout.CENTER);
+
 
     }
 
@@ -49,6 +60,10 @@ public class View extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public void selectedTabChanged() {
 
     }
 }
